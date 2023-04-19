60fc89352250fddb7a19751ce982cc356e644407
Vicente Viera <plantedcoot706@gmail.com>
2023-04-19 17:49:02 -0600

    .env added. Future commit delition

--- powerdash/tailwind.config.ts
+++ powerdash/tailwind.config.ts
@@ -3,6 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "orange": "#F15025",
+      "white": "#FFFFFF",
+      "gray": "#CED0CE",
+      "lgray": "#E6E8E6",
+      "black": "#191919"
+    },
     extend: {},
   },
   plugins: [],
