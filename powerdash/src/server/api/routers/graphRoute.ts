e3b9aa863f38b7abfb00dd5ff2a965f900711da2
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2023-06-12 09:45:23 -0700

    buildea esta mierda

--- powerdash/src/server/api/routers/graphRoute.ts
+++ powerdash/src/server/api/routers/graphRoute.ts
@@ -6,39 +6,43 @@ import { Console } from "console";
 
 const prisma = new PrismaClient();
 
-function getRandom(min: number, max: number){
-    min = Math.ceil(min);   
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandom(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 interface ReturnData {
-    [label: number]: number;
+  [label: number]: number;
 }
 
 export type { ReturnData };
 
 export const graphRouter = createTRPCRouter({
-    dataGraph: publicProcedure
-        .input(z.object({
-            xAxis: z.string(),
-            yAxis: z.string(),
-            type: z.string(),
-            selects: z.number().nonnegative().min(0)
-        }))
-        .mutation(async (req) => {
-            const { input: {selects} } = req;
-            //Debido a que no tenemos tiempo para recopilar los datos y procesarlos optamos por regresar dummy data
-            //Si se llegase a dar el tiempo se deberian de hacer 2 consultas a la base de datos y contar cuantos valores regresa
-            const xValue = getRandom(100, 500);
-            const yValue = getRandom(100, 500);
-            const values = {xValue, yValue};
+  dataGraph: publicProcedure
+    .input(
+      z.object({
+        xAxis: z.string(),
+        yAxis: z.string(),
+        type: z.string(),
+        selects: z.number().nonnegative().min(0),
+      })
+    )
+    .mutation(async (req) => {
+      const {
+        input: { selects },
+      } = req;
+      //Debido a que no tenemos tiempo para recopilar los datos y procesarlos optamos por regresar dummy data
+      //Si se llegase a dar el tiempo se deberian de hacer 2 consultas a la base de datos y contar cuantos valores regresa
+      const xValue = getRandom(100, 500);
+      const yValue = getRandom(100, 500);
+      const values = { xValue, yValue };
 
-            let data: ReturnData = {};
+      const data: ReturnData = {};
 
-            for(let i=0;i<selects;i++){
-                data[`${i}`] = getRandom(100, 500);
-            }
+      for (let i = 0; i < selects; i++) {
+        data[`${i}`] = getRandom(100, 500);
+      }
 
-            return data;
-        })
+      return data;
+    }),
 });
